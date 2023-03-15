648ec31202a26fcafac384076799bd0d6d0fe230
Lucas Amiaud <lamiaud@coreoz.com>
2023-03-15 16:30:48 +0100

    fix location context redirection hooks

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -4,10 +4,10 @@ export { SsrWritableObservable } from './lib/observable/SsrWritableObservable';
 export type { SsrConfigValue, SsrObservableContent } from './lib/observable/SsrWritableObservable';
 export { useObservable } from './lib/observable/useObservable';
 
-export type { SsrLocationContext } from './lib/locationContext/SsrLocationContext';
-export { SsrLocationContextHolder } from './lib/locationContext/SsrLocationContext';
-export { redirect } from './lib/locationContext/Redirect/Redirect';
-export { Navigate, useNavigate } from './lib/locationContext/Navigate/Navigate';
+export type { SsrLocationContext } from './lib/locationcontext/SsrLocationContext';
+export { SsrLocationContextHolder } from './lib/locationcontext/SsrLocationContext';
+export { redirect } from './lib/locationcontext/redirect/Redirect';
+export { Navigate, useNavigate } from './lib/locationcontext/navigate/Navigate';
 
 export type { BrowserRenderOption } from './lib/renderer/BrowserApplicationRenderer';
 export { renderBrowserApplication, extractMonitorContextData } from './lib/renderer/BrowserApplicationRenderer';
